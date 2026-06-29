623024b31a68d1d0707bde632ba79a885577b119
sebastianFeltes <jikansui16@gmail.com>
2026-06-29 19:46:57 -0300

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-2xl font-bold font-nunito text-custom-azul-oscuro'>App CFL404</h1>
+      <p className='text-custom-gris-claro font-nunito'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus natus ullam perspiciatis laborum, doloremque cumque minus temporibus labore saepe rerum? Quam, quidem. Eaque, maxime? Eaque doloremque eos voluptatem voluptas voluptates.</p>
+    </>
+  )
+}
+
+export default App
